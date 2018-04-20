d73f20622d522e0498a640e1726932de7664015e
Michael Groesbeck <mdgbeck@gmail.com>
2018-04-20 09:32:26 -0400

    add pub theme with title formatting

--- R/ggplot_themes.R
+++ R/ggplot_themes.R
@@ -1,3 +1,37 @@
+#' Personal Custom Theme with Titles
+#'
+#' Theme based on the \code{ggplot2 theme_bw} with modified font (UNIX use only). Thicker title case than plain \code{theme_mdgr.}
+#'
+#' @import ggplot2
+
+#' @param base_size base font size
+#' @param base_family base font family
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(mtcars) + geom_point(aes(wt, mpg)) + theme_mdgrpub()
+theme_mdgrpub <- function(base_size = 12, base_family = "Lato"){
+
+  ggplot2::theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    ggplot2::theme(
+      panel.background = element_blank(),
+      plot.background = element_blank(),
+      legend.background = element_blank(),
+      legend.key = element_blank(),
+      panel.grid.major = element_line(color = "gray85", size = .3),
+      panel.grid.minor = element_line(color = "gray89", size = .2),
+      panel.border = element_rect(fill = NA, color = "gray31"),
+      axis.ticks = element_blank(),
+      strip.background = element_blank(),
+      strip.text = element_text(size = rel(1))) +
+    theme(
+      plot.title = element_text(family = "Lato Medium"),
+      axis.text = element_text(family = "Lato Light")
+    )
+
+}
 
 
 
@@ -13,7 +47,7 @@
 #' @export
 #'
 #' @examples
-#' library('ggplot2)
+#' library(ggplot2)
 #' ggplot(mtcars) + geom_point(aes(wt, mpg)) + theme_mdgr()
 theme_mdgr <- function(base_size = 13, base_family = "Lato Light"){
 
@@ -46,7 +80,7 @@ theme_mdgr <- function(base_size = 13, base_family = "Lato Light"){
 #' @export
 #'
 #' @examples
-#' library('ggplot2)
+#' library(ggplot2)
 #' ggplot(mtcars) + geom_point(aes(wt, mpg)) + theme_mdgbeck()
 theme_mdgbeck <- function(base_size = 11){
 
